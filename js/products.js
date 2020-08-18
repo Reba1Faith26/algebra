a34ab88a339d8ac1a525fd8b8aed6f8b8f023636
Reba1Faith26 <67510270+Reba1Faith26@users.noreply.github.com>
2020-08-17 23:27:35 -0700

    Add files via upload

--- js/products.js
+++ js/products.js
@@ -9,12 +9,18 @@ function calculation(){
         var negative_coefficient = Math.sqrt(perfect_sqr.value)*-1;
         var x1 = term_x.value.charAt(0);
         var x2 = term_x.value.charAt(0);
-        if(perfect_sqr.value == 0){
-            answer.innerText = "answer = (" + x1 + "-" + negative_coefficient + ") + (" + x2 + "+" + positive_coefficient +")";
+        if(perfect_sqr.value!=0){
+            if(Number.isInteger(Math.sqrt(perfect_sqr.value))){
+                answer.innerText = "answer = (" + x1 + "+" + positive_coefficient +") + (" + x2 + negative_coefficient + ")";
+            }
+            else{
+                answer.innerText = "the number is not a perfect square ,a perfect square is a number that return an integer when inserted in a square root";
+            }
         }
         else{
-            answer.innerText = "answer = (" + x1 + negative_coefficient + ") + (" + x2 + "+" + positive_coefficient +")";
+            answer.innerText = " the textbox after the negative sign is empty enter a perfect square value";
         }
     }
 }
+
 calculation();
